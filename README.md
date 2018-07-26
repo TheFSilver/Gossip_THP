4ef1d42ca193b7c9c04b1453730799cbd5af39b7
TheFSilver <dasylva.f@gmail.com>
2018-07-26 23:38:06 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ze Super Gossip_Project
+
+
+Salut mon p'tit lapin ! Bienvenue sur le repository de la superbe application Rails de François Dasylva et Jeremy Rak !
+
+## Instructions ##
+
+Pour tester notre super appli, réalisez les processus suivants :
+
+- git clone https://github.com/skageraz/Gossip_THP pour copier notre répo sur votre machine
+- cd THPBlog dans votre ligne de commande
+- Executez bundle install pour mettre à jour votre liste de gems
+- Executez rails db:migrate pour que les migrations soient bien effectuées
+- Executez rails db:seed pour pouvoir initialiser et remplir votre database !
+
+### Concepteurs ###
+
+Conçu en Ruby on Rails, ligne par ligne, par Jeremy R. ( <a href="https://github.com/skageraz">Skageraz</a> ) et François D. ( <a href="https://github.com/TheFSilver">TheFSilver</a> ).
---
+++ db/seeds.rb
@@ -0,0 +1,71 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+def ignite
+  10.times do
+    City.create(
+      name: Faker::Address.city
+      )
+
+    User.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      description: Faker::ChuckNorris.fact,
+      email: Faker::Internet.email,
+      age: Faker::Number.between(10, 75),
+      city_id: rand(City.first.id..City.last.id)
+        )
+  end
+end
+
+def create_gossips
+  20.times do
+    Gossip.create(
+      title: Faker::Book.title,
+      content: Faker::DumbAndDumber.quote,
+      date: Faker::Date.backward(14),
+      user_id: rand(User.first.id..User.last.id)
+   )
+  end
+end
+
+def create_tags_and_through_table_gossip
+  10.times do
+    Tag.create(
+      title: Faker::Book.title
+    )
+  end
+
+    Gossip.all.each do |gossip|
+  gossip.tags.push(Tag.all.sample)
+  gossip.save
+end
+
+# give each assembly a random part
+  Tag.all.each do |tag|
+  tag.gossips.push(Gossip.all.sample)
+  tag.save
+end
+end
+
+def create_PrivateMessage
+   PrivateMessage.create(
+     content: Faker::DumbAndDumber.quote,
+     date: Faker::Date.backward(14),
+     sender_id: rand(User.first.id..User.last.id),
+     recipient_id: rand(User.first.id..User.last.id)
+   )
+ end
+
+   comment = Comment.create(
+     content: Faker::Lorem.sentences(1),
+     user_id: rand(User.first.id..User.last.id),
+     gossip_id: rand(Gossip.first.id..Gossip.last.id)
+   )
